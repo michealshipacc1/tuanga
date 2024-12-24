0f3250ab8fc5fb9bd39fa38342a72f2275fffcdb
michealshipacc1 <139836537+michealshipacc1@users.noreply.github.com>
2024-12-24 10:49:17 +0700

    Update LinkedListExample1.java

--- 19JSP02/src/jsp/examples/collections/LinkedListExample1.java
+++ 19JSP02/src/jsp/examples/collections/LinkedListExample1.java
@@ -15,7 +15,7 @@ public class LinkedListExample1 {
         // Create linkedList
         LinkedList<String> linkedList = new LinkedList<String>();
         // Add objects to linkedList
-        linkedList.add("Java");
+        linkedList.add("Java111");
         linkedList.add("C++");
         linkedList.add("PHP");
         linkedList.add("Java");
