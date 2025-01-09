1b9f86484ce46fae1497288a544bc2e883e21306
michealshipacc1 <139836537+michealshipacc1@users.noreply.github.com>
2025-01-09 10:08:06 +0700

    Update CollectionExample1.java
    remove commment

--- 19JSP02/src/jsp/examples/collections/CollectionExample1.java
+++ 19JSP02/src/jsp/examples/collections/CollectionExample1.java
@@ -19,7 +19,7 @@ public class CollectionExample1 {
 	public static void main(String[] args) {
         List<String> arrayList = new ArrayList<String>();
         arrayList.add("PHP1231");
-        arrayList.add("Pyt123hon1");
+        arrayList.add("Python1");
         arrayList.add("Java1231");
         arrayList.add("C++");
         System.out.println("Các phần tử của ArrayList");
